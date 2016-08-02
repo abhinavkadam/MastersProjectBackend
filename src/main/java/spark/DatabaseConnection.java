0c815ffb16fa71c9944f3b86772237d3cfd03a57
Abhinav Kadam <abhinavkadam@gmail.com>
2016-08-02 01:25:55 -0700

    Create DatabaseConnection.java

---
+++ src/main/java/spark/DatabaseConnection.java
@@ -0,0 +1,50 @@
+package spark;
+
+/* 
+** 	@author abhinav kadam 
+*/
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import scala.runtime.AbstractFunction0;
+
+/* 
+** This class handles and encapsulates the data related to database connection.	 
+*/
+public class DatabaseConnection extends AbstractFunction0<Connection> implements Serializable{
+	private String driverClassName;
+	private String connectionUrl;
+	private String username;
+	private String password;
+	
+	public DatabaseConnection(String driverClassName, String connectionUrl, String username, String password) {
+		this.driverClassName = driverClassName;
+		this.connectionUrl = connectionUrl;
+		this.username = username;
+		this.password = password;
+	}
+	
+	@Override
+	public Connection apply() {
+		try {
+			Class.forName(driverClassName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Properties props = new Properties();
+		props.setProperty("user", username);
+		props.setProperty("password", password);
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(connectionUrl, props);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}		
+}
