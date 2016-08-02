48c3b546b113a2e203e7ecc12afc22ddebe114b2
Abhinav Kadam <abhinavkadam@gmail.com>
2016-08-02 01:01:56 -0700

    Create SystemConstants.java

---
+++ src/main/java/com/edu/util/SystemConstants.java
@@ -0,0 +1,18 @@
+/* 
+** 	@author Abhinav Kadam 
+*/
+package com.edu.util;
+
+/* 
+** 	This class defines the system constants used in the project. 
+*/
+public class SystemConstants {
+
+	public static final String DB_USERNAME = "db_username";
+	public static final String DB_PASSWORD = "db_password";
+	public static final String DB_URL = "db_url";
+	
+	public static final String KEY = "key";
+	public static final String VALUE = "value";
+	
+}
